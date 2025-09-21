72299dd96f95ea404e3bfc8f92533535a9242f76
Ramiro Perichon <perichonramiro12@gmail.com>
2025-09-21 11:41:24 -0300

    se añadieron las funciones js para dar interactividad a la pagina

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
+ let amigos = [];
 
+ function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === ""){
+        alert("El nombre no puede estar vacío");
+        return;
+    }
+    
+    amigos.push(nombre);
+    input.value = "";
+    mostrarAmigos();
+ }
+    
+ function mostrarAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach(function(amigo) {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+
+ }
+
+ function sortearAmigo() {
+    if(amigos.length === 0) {
+        alert("No hay amigos para sortear");
+        return;
+    }
+
+    const indice = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indice];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>🎉 El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+ }
